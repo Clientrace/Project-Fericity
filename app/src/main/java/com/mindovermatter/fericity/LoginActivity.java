4ef27075bd2d451f557a10496ce223ba88ddbf91
Kim Clarence Peñaflor <pkimclarence@yahoo.com.ph>
2016-05-21 14:32:25 +0800

    Login Added

--- app/src/main/java/com/mindovermatter/fericity/LoginActivity.java
+++ app/src/main/java/com/mindovermatter/fericity/LoginActivity.java
@@ -1,17 +1,59 @@
 package com.mindovermatter.fericity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                if(profile!=null){
+                    UserProfile.fname = profile.getFirstName();
+                    UserProfile.mname = profile.getMiddleName();
+                    UserProfile.lname = profile.getMiddleName();
+                }
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
+
 }
---
+++ app/src/main/java/com/mindovermatter/fericity/UserProfile.java
@@ -0,0 +1,12 @@
+package com.mindovermatter.fericity;
+
+/**
+ * Created by Sherlock Holmes on 5/21/2016.
+ */
+public class UserProfile {
+    public static String fname = "";
+    public static String mname = "";
+    public static String lname = "";
+    public static int numFriendsOnline = 0;
+
+}
